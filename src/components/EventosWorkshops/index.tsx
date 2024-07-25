48d9d0dbb23c8c69c620510a793833a2d60d3383
duccini <guilhermeduccini@gmail.com>
2024-07-25 09:24:18 -0300

    fix component work

--- src/components/EventosWorkshops/index.tsx
+++ src/components/EventosWorkshops/index.tsx
@@ -1,4 +1,4 @@
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
 const EventosWorkshops = () => {
   return (
@@ -8,32 +8,52 @@ const EventosWorkshops = () => {
         <div className={styles.texts}>
           <ul>
             <li>
-              <svg className={styles.icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> //add simbolo de check 'V'
+              <svg
+                className={styles.icon}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
                 <path d="M9 19.4l-8.1-8.1 1.4-1.4 6.7 6.7L21.7 4.1 23 5.5z" />
               </svg>
               Introdução à Programação com Scratch para Crianças
             </li>
             <li>
-              <svg className={styles.icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <svg
+                className={styles.icon}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
                 <path d="M9 19.4l-8.1-8.1 1.4-1.4 6.7 6.7L21.7 4.1 23 5.5z" />
               </svg>
               Empreendedorismo Tech para Jovens Negros
             </li>
             <li>
-              <svg className={styles.icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <svg
+                className={styles.icon}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
                 <path d="M9 19.4l-8.1-8.1 1.4-1.4 6.7 6.7L21.7 4.1 23 5.5z" />
               </svg>
               Liderança Feminina em Tecnologia
             </li>
             <li>
-              <svg className={styles.icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <svg
+                className={styles.icon}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
                 <path d="M9 19.4l-8.1-8.1 1.4-1.4 6.7 6.7L21.7 4.1 23 5.5z" />
               </svg>
               Desenvolvimento de Software Acessível
             </li>
           </ul>
         </div>
-        <img src="/images/img-eventos.png" alt="Eventos e Workshops" className={styles.image} />
+        <img
+          src="/images/img-eventos.png"
+          alt="Eventos e Workshops"
+          className={styles.image}
+        />
       </div>
     </section>
   );
