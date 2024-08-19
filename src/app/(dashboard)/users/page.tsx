75750618c21639c204d5450dbef4e54284386a7c
Felipe Maia <felipemaia850@gmail.com>
2024-08-19 16:10:41 -0300

    feat: add `DashboardHeader` component

--- src/app/(dashboard)/users/page.tsx
+++ src/app/(dashboard)/users/page.tsx
@@ -1,30 +1,12 @@
-import Image from "next/image";
+import DashboardHeader from "../_components/DashboardHeader";
 import UsersTable from "./_components/UsersTable";
-import profileImg from "public/images/Baylee.svg";
-import { MdKeyboardArrowDown } from "react-icons/md";
 
 import styles from "./page.module.css";
 
 export default function Users() {
   return (
     <main className={styles.users}>
-      <header className={styles.dashboardHeader}>
-        <h1 className={styles.title}>
-          <span> Painel Administrativo / </span>Usuários
-        </h1>
-        <div className={styles.profile}>
-          <Image
-            src={profileImg}
-            alt="Imagem do Perfil"
-            width={24}
-            height={24}
-            quality={100}
-            priority
-          />
-          <p>Baylee Horne</p>
-          <MdKeyboardArrowDown size={18} />
-        </div>
-      </header>
+      <DashboardHeader text="Usuários" />
 
       <UsersTable />
     </main>
