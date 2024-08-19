6ba159008545e9c2b42a970e5c3fa70cb6c47b02
Felipe Maia <felipemaia850@gmail.com>
2024-08-18 23:17:08 -0300

    feat: add profile to dashboard header

--- src/app/(dashboard)/users/page.tsx
+++ src/app/(dashboard)/users/page.tsx
@@ -1,12 +1,30 @@
+import Image from "next/image";
 import UsersTable from "./_components/UsersTable";
+import profileImg from "public/images/Baylee.svg";
+import { MdKeyboardArrowDown } from "react-icons/md";
+
 import styles from "./page.module.css";
 
 export default function Users() {
   return (
     <main className={styles.users}>
-      <h1 className={styles.title}>
-        <span> Painel Administrativo / </span>Usuários
-      </h1>
+      <header className={styles.dashboardHeader}>
+        <h1 className={styles.title}>
+          <span> Painel Administrativo / </span>Usuários
+        </h1>
+        <div className={styles.profile}>
+          <Image
+            src={profileImg}
+            alt="Imagem do Perfil"
+            width={24}
+            height={24}
+            quality={100}
+            priority
+          />
+          <p>Baylee Horne</p>
+          <MdKeyboardArrowDown size={18} />
+        </div>
+      </header>
 
       <UsersTable />
     </main>
