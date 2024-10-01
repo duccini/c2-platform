4dee9d120e4f048783da462a8a2ce0c9a180c9b2
Jefferson Nunes <nunesjefferson425@gmail.com>
2024-10-01 20:56:12 -0300

    context

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -1,20 +1,25 @@
-import { createContext, useState, useContext, ReactNode } from 'react';
+"use client"
+import React, { createContext, useState, useContext, ReactNode } from 'react';
 
-interface User {
+// Definindo o tipo de usuário
+type UserType = {
   username: string;
   email: string;
   token: string;
-}
+};
 
-interface UserContextProps {
-  user: User | null;
-  setUser: (user: User | null) => void;
-}
+// Tipagem para o contexto
+type UserContextType = {
+  user: UserType | null; // O usuário pode ser nulo inicialmente
+  setUser: React.Dispatch<React.SetStateAction<UserType | null>>;
+};
 
-const UserContext = createContext<UserContextProps | undefined>(undefined);
+// Criando o contexto com valor inicial como `null`
+const UserContext = createContext<UserContextType | undefined>(undefined);
 
+// Criando o provider
 export const UserProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<UserType | null>(null);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
@@ -23,10 +28,11 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
   );
 };
 
-export const useUser = (): UserContextProps => {
+// Hook para acessar o contexto
+export const useUser = (): UserContextType => {
   const context = useContext(UserContext);
   if (!context) {
-    throw new Error('useUser must be used within a UserProvider');
+    throw new Error("useUser must be used within a UserProvider");
   }
   return context;
 };
